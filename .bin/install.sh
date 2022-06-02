fb8a957f03cafba4ef6ac769b79ae3a78ad7f089
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2022-06-02 09:03:08 +0900

    fix: wrong option for test existance of symlink

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/zsh
 
-set -ue
+set -ue pipefail
 
 helpmsg() {
     command echo "Usage: $0 [--help| -h]" 0>&2
@@ -32,7 +32,7 @@ link_to_homedir() {
         [[ $(basename $f) == ".git" ]] && continue
 
         # remove the symbolic link being generated
-        if [[ -e "$HOME/$(basename $f)" ]]; then
+        if [[ -L "$HOME/$(basename $f)" ]]; then
             command rm -f "$HOME/$(basename $f)"
         fi
 
@@ -41,10 +41,9 @@ link_to_homedir() {
             command mv "$HOME/$(basename $f)" "$HOME/$backup_dir"
         fi
 
-        command echo "$f -> $HOME/$(basename $f)"
-        command ln -snf "$f" "$HOME/$(basename $f)"
+        command echo "$HOME/$(basename $f) -> $f"
+        command ln -snf $f $HOME
     done
-
 }
 
 while [ $# -gt 0 ]; do
