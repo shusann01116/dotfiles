41111091df991d51d1a50ee775771d78cf785dca
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2022-06-02 09:01:31 +0900

    refactor: removed unnecesarry code

--- test/installer.sh
+++ test/installer.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 set -ue
 
@@ -6,5 +6,4 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd -P)"
 dot_dir="$(dirname ${script_dir})"
 
 ${dot_dir}/.bin/install.sh
-ls -la ${HOME}
 source ${HOME}/.zshrc
