5bf32a145706c739dde149cd31c0ff7132dd384b
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2022-06-21 20:54:56 +0900

    refactor: add test script ensures the packages installed

--- test/installer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-set -ue
-
-script_dir="$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd -P)"
-dot_dir="$(dirname ${script_dir})"
-
-${dot_dir}/.bin/install.sh
-source ${HOME}/.zshrc
---
+++ test/test.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+starship --version
