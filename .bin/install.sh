3aa5125b067f5771d704a670379982283bd276a2
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2022-07-14 03:47:00 +0900

    feat: Add lint for shell, cue and md (#11)
    * style: Remove command
    
    *feat: Add md lint
    
    * feat: Add shellcheck
    
    * refactor: Set workdir option to docker.#Run in mdlint
    
    * style: Format cue files
    
    * feat: Add cue lint
    
    * feat: Cache build file
    
    * fix: Cue lint to work
    
    * style: Edit files to pass the lints

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,18 +1,16 @@
-#!/bin/zsh
-
-set -ue pipefail
+#!/bin/bash
 
 helpmsg() {
-    command echo "Usage: $0 [--help| -h]" 0>&2
-    command echo ""
+    echo "Usage: $0 [--help| -h]" 0>&2
+    echo ""
 }
 
 link_to_homedir() {
     local backup_dir=".dotbackup"
-    command echo "backing up dotfiles..."
+    echo "backing up dotfiles..."
 
     if [ ! -d "$HOME/$backup_dir" ]; then
-        command echo "creating $HOME/$backup_dir folder"
+        echo "creating $HOME/$backup_dir folder"
         mkdir -p "$HOME/$backup_dir"
     fi
 
@@ -22,27 +20,27 @@ link_to_homedir() {
 
     # get the name of parent direcotry of script directory
     local dotdir
-    dotdir=$(dirname ${script_dir})
+    dotdir=$(dirname "${script_dir}")
 
     # exit the function when the direcotry is invalid
     [[ "$HOME" == "$dotdir" ]] && exit 1
 
     for f in "$dotdir"/.??*; do
         # skip for .git folder
-        [[ "$(basename $f)" == ".git" ]] && continue
+        [[ "$(basename "$f")" == .git ]] && continue
 
         # remove the symbolic link being generated
-        if [[ -L "$HOME/$(basename $f)" ]]; then
-            command rm -f "$HOME/$(basename $f)"
+        if [[ -L "$HOME"/"$(basename "$f")" ]]; then
+            rm -f "$HOME"/"$(basename "$f")"
         fi
 
         # backup the file being overided in home directory
-        if [[ -e "$HOME/$(basename $f)" ]]; then
-            command mv "$HOME/$(basename $f)" "$HOME/$backup_dir"
+        if [[ -e "$HOME"/"$(basename "$f")" ]]; then
+            mv "$HOME"/"$(basename "$f")" "$HOME"/"$backup_dir"
         fi
 
-        command echo "$HOME/$(basename $f) -> $f"
-        command ln -snf $f $HOME
+        echo "$HOME"/"$(basename "$f")" - >"$f"
+        ln -snf "$f" "$HOME"
     done
 }
 
@@ -53,10 +51,10 @@ installPyenv() {
 
 installStarShip() {
     echo "Installing StarShip"
-    mkdir ${HOME}/tmp
-    curl -sS https://starship.rs/install.sh >${HOME}/tmp/install.sh && chmod a+x ${HOME}/tmp/install.sh
-    ${HOME}/tmp/install.sh --yes
-    echo 'eval "$(starship init zsh)"' >>$HOME/.zshrc
+    mkdir "${HOME}"/tmp
+    curl -sS https://starship.rs/install.sh >"${HOME}"/tmp/install.sh && chmod a+x "${HOME}"/tmp/install.sh
+    "${HOME}"/tmp/install.sh --yes
+    echo "eval \"$(starship init zsh)\"" >>"$HOME"/.zshrc
     rm -rf ~/tmp
 }
 
---
+++ ci/cue/lint.cue
@@ -0,0 +1,59 @@
+package cue
+
+import (
+	"list"
+
+	"dagger.io/dagger"
+
+	"universe.dagger.io/bash"
+	"universe.dagger.io/docker"
+)
+
+#Lint: {
+	contents:       dagger.#FS
+	version:        string | *"latest"
+	go_version:     string | *"1.18.4"
+	alpine_version: string | *"3.16"
+
+	packages: ["git", "bash"]
+
+	_build: docker.#Build & {
+		steps: [
+			docker.#Pull & {
+				source: "golang:\(go_version)-alpine\(alpine_version)"
+			},
+			docker.#Run & {
+				command: {
+					name: "apk"
+					args: list.Concat([["add"], packages])
+					flags: {
+						"-U":         true
+						"--no-cache": true
+					}
+				}
+			},
+			docker.#Run & {
+				command: {
+					name: "sh"
+					args: ["-c", """
+                            go install cuelang.org/go/cmd/cue@\(version)
+                            """]
+				}
+			},
+		]
+	}
+
+	bash.#Run & {
+		input: _build.output
+		mounts: src: {
+			dest:       "/src"
+			"contents": contents
+		}
+		workdir: "/src"
+		script: contents: #"""
+			find . -name '*.cue' -not -path '*/cue.mod/*' -print | time xargs -n 1 -P 8 cue fmt -s
+			modified="$(git status -s . | grep -e "^ M"  | grep "\.cue" | cut -d ' ' -f3 || true)"
+			test -z "$modified" || (echo -e "linting error in:\n${modified}" > /dev/stderr ; false)
+			"""#
+	}
+}
