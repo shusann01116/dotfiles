6da0eb415d3493e15c5c43615837c73fd5c39af3
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2022-09-29 20:32:03 +0900

    syntax highlighting (tree-sitter)

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -22,4 +22,8 @@ packer.startup(function(use)
   use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's build-in LSP
   use 'hrsh7th/nvim-cmp' -- Completion
   use 'neovim/nvim-lspconfig' -- LSP
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
 end)
