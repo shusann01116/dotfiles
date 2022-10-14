cab30b184c4d89647c5277524bc060cb2c2484ec
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2022-10-14 22:42:03 +0900

    chore: Restruct and add config for nvim (#4)
    This commit removes most of the previouse files.
    
    * reset dotfiles
    
    * move several files
    
    * add gitignore
    
    * add init.lua
    
    * Squashed commit of the following:
    
    commit a595807ab0b60836713d33b67dabd3be70a725ea
    Author: shusann01116 <26602565+shusann01116@users.noreply.github.com>
    Date:   Wed Sep 28 17:27:01 2022 +0000
    
        ln to home dir from this repo doesn't take an effect
    
    commit 80f427ceabd1ec88c5d20d11961841b3e7927ee4
    Author: shusann01116 <26602565+shusann01116@users.noreply.github.com>
    Date:   Wed Sep 28 17:08:40 2022 +0000
    
        symlinked .tmux.conf represent in home dir
    
    commit a4fc794a9652324cb158772227a76ff2608469d0
    Author: shusann01116 <26602565+shusann01116@users.noreply.github.com>
    Date:   Wed Sep 28 17:03:12 2022 +0000
    
        add tmux conf repository
    
    * fix symlink for tmux conf
    
    * base configuration
    
    * add highlights to config
    
    * delink .tmux.conf.local
    
    * change prefix to C-s
    
    * add yank support for macOS and Windows
    
    * add plugins config
    
    * change symlink to file to support all machine
    
    * fix tmux backspace behaves strange on macos
    
    * add color theme
    
    * add completion
    
    * syntax highlighting (tree-sitter)
    
    * autotag
    
    * add telecope
    
    * - add devicons
    - add cue lang syntax highlights
    - add dagger lsp
    
    * add yaml schema for highlighting
    
    * fix bufferline tab rendering issue
    
    * feat: add modules
    
    - web-devicons
    - nvim-colorizer for RGB number
    - lspsaga for lsp UIs (not working for now)
    
    * feat: install shell script
    
    * fix (telescope): typo with file_browser initial mode
    
    * feat: new plugins
    
    - git
    - mason for lsp management
    - prettier for codeformatting
    
    * restore file from main branch
    
    * remove install.sh

---
+++ home/.config/nvim/after/plugin/treesitter.rc.lua
@@ -0,0 +1,43 @@
+local status, ts = pcall(require, 'nvim-treesitter.configs')
+if (not status) then return end
+
+-- This is required to not have cue files marked as `cuesheet`
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  pattern = { "*.cue" },
+  command = "set filetype=cue"
+})
+
+local parse_config = require "nvim-treesitter.parsers".get_parser_configs()
+parse_config.cue = {
+  install_info = {
+    url = "https://github.com/eonpatapon/tree-sitter-cue",
+    files = { "src/parser.c", "src/scanner.c" },
+    branch = "main"
+  },
+  filetype = "cue",
+}
+
+ts.setup {
+  highlight = {
+    enable = true,
+    disable = {},
+  },
+  indent = {
+    enable = true,
+    disable = {},
+  },
+  ensure_installed = {
+    'tsx',
+    'lua',
+    'json',
+    'css',
+    'hcl',
+    'cue',
+    'c_sharp',
+    'yaml',
+    'bash',
+  },
+  autotag = {
+    enable = true,
+  }
+}
