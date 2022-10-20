0fe967148665aac090c1aced531630808175062e
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2022-10-20 11:39:50 +0000

    - add yamlls and prettier - set relativeline

--- home/.config/nvim/after/plugin/mason.rc.lua
+++ home/.config/nvim/after/plugin/mason.rc.lua
@@ -1,6 +1,7 @@
 local status, mason = pcall(require, "mason")
 if (not status) then return end
 local status2, lspconfig = pcall(require, "mason-lspconfig")
+if (not status2) then return end
 
 mason.setup {}
 lspconfig.setup {
@@ -12,3 +13,14 @@ lspconfig.setup {
 
 require 'lspconfig'.tailwindcss.setup {}
 require 'lspconfig'.dagger.setup {}
+require 'lspconfig'.dockerls.setup {}
+require 'lspconfig'.yamlls.setup {
+  settings = {
+    yaml = {
+      schemas = {
+        ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "**/*docker-compose.yml",
+        ["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] = "**/kube-manifests/*",
+      }
+    }
+  }
+}
--- home/.config/nvim/lua/base.lua
+++ home/.config/nvim/lua/base.lua
@@ -10,7 +10,7 @@ vim.opt.title = true
 vim.opt.autoindent = true
 vim.opt.hlsearch = true
 vim.opt.backup = false
-vim.opt.showcmd =true
+vim.opt.showcmd = true
 vim.opt.cmdheight = 1
 vim.opt.laststatus = 2
 vim.opt.expandtab = true
@@ -23,12 +23,13 @@ vim.opt.smarttab = true
 vim.opt.breakindent = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
-vim.opt.ai =true -- Auto indent
+vim.opt.ai = true -- Auto indent
 vim.opt.si = true -- Smart indent
 vim.opt.wrap = false -- No wrap lines
 vim.opt.backspace = 'start,eol,indent'
 vim.opt.path:append { '**' } -- Finding files - Search down into subfolders
 vim.opt.wildignore:append { '*/node_modules/*' }
+vim.opt.relativenumber = true
 
 
 -- Undercurl
@@ -43,4 +44,3 @@ vim.api.nvim_create_autocmd("InsertLeave", {
 
 -- Add asterisks in block comments
 vim.opt.formatoptions:append { 'r' }
-
