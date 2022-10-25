ac7a30274cac48f0a0011e44f0cf65c29a22e207
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2022-10-25 23:36:43 +0900

    remove lspcmd

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -26,9 +26,6 @@ packer.startup(function(use)
   use 'L3MON4D3/LuaSnip' -- Snippet
   use 'glepnir/lspsaga.nvim' -- LSP UIs
   use 'onsails/lspkind-nvim' -- vscode-like pictograms
-  use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
-  use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's build-in LSP
-  use 'hrsh7th/nvim-cmp' -- Completion
   use 'neovim/nvim-lspconfig' -- LSP
   use {
     'nvim-treesitter/nvim-treesitter',
