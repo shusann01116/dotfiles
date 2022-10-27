1b80de1634b1cacb737084b24330df54035968d7
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2022-10-27 16:00:24 +0900

    add coq.thirdparty capabilities

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -1,55 +1,52 @@
-local status, packer = pcall(require, 'packer')
-if (not status) then
-  print("Packer is not installed")
-  return
+local status, packer = pcall(require, "packer")
+if not status then
+	print("Packer is not installed")
+	return
 end
 
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
 packer.startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use {
-    'svrana/neosolarized.nvim',
-    requires = { 'tjdevries/colorbuddy.nvim' }
-  }
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  } -- Statusline
+	use("wbthomason/packer.nvim")
+	use({
+		"svrana/neosolarized.nvim",
+		requires = { "tjdevries/colorbuddy.nvim" },
+	})
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	}) -- Statusline
 
-  use 'nvim-tree/nvim-tree.lua'
+	use("nvim-tree/nvim-tree.lua")
 
-  use 'williamboman/mason.nvim' -- Package manager
-  use 'williamboman/mason-lspconfig.nvim' -- Mason integration with lspconfig
+	use("williamboman/mason.nvim") -- Package manager
+	use("williamboman/mason-lspconfig.nvim") -- Mason integration with lspconfig
 
-  use 'kyazdani42/nvim-web-devicons' -- File icons
-  use 'L3MON4D3/LuaSnip' -- Snippet
-  use 'glepnir/lspsaga.nvim' -- LSP UIs
-  use 'onsails/lspkind-nvim' -- vscode-like pictograms
-  use 'neovim/nvim-lspconfig' -- LSP
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate'
-  }
-  use 'jose-elias-alvarez/null-ls.nvim' -- Use neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
+	use("kyazdani42/nvim-web-devicons") -- File icons
+	use("L3MON4D3/LuaSnip") -- Snippet
+	use("glepnir/lspsaga.nvim") -- LSP UIs
+	use("onsails/lspkind-nvim") -- vscode-like pictograms
+	use("neovim/nvim-lspconfig") -- LSP
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+	})
+	use("jose-elias-alvarez/null-ls.nvim") -- Use neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
 
-  use {
-    'ms-jpq/coq_nvim',
-    branch = 'coq',
-    requires = { 'ms-jpq/coq.artifacts', branch = 'artifacts' }
-  }
+	use("ms-jpq/coq_nvim")
+	use("ms-jpq/coq.artifacts")
+	use("ms-jpq/coq.thirdparty")
 
-  use 'windwp/nvim-autopairs'
-  use 'windwp/nvim-ts-autotag'
+	use("windwp/nvim-autopairs")
+	use("windwp/nvim-ts-autotag")
 
-  use 'nvim-lua/plenary.nvim' -- Common utilities
-  use 'nvim-telescope/telescope.nvim'
-  use 'nvim-telescope/telescope-file-browser.nvim'
+	use("nvim-lua/plenary.nvim") -- Common utilities
+	use("nvim-telescope/telescope.nvim")
+	use("nvim-telescope/telescope-file-browser.nvim")
 
-  use 'akinsho/bufferline.nvim'
-  use 'norcalli/nvim-colorizer.lua'
+	use("akinsho/bufferline.nvim")
+	use("norcalli/nvim-colorizer.lua")
 
-  use 'lewis6991/gitsigns.nvim'
-  use 'dinhhuy258/git.nvim' -- For git blame & brows
-
+	use("lewis6991/gitsigns.nvim")
+	use("dinhhuy258/git.nvim") -- For git blame & brows
 end)
