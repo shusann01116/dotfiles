31967f539a89a5757fe60fca5fb751f453f12a60
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2022-10-29 13:49:43 +0900

    Add capability to select whether formatter on save
    refer: https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Avoiding-LSP-formatting-conflicts

--- home/.config/nvim/after/plugin/null-ls.rc.lua
+++ home/.config/nvim/after/plugin/null-ls.rc.lua
@@ -1,53 +1,71 @@
 local status, null_ls = pcall(require, "null-ls")
 if not status then
-  return
+	return
+end
+
+local lsp_formatting = function(bufnr)
+	vim.lsp.buf.format({
+		filter = function(client)
+			return client.name == "null-ls"
+		end,
+		bufnr = bufnr,
+	})
+end
+
+local lsp_augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+local on_attach = function(client, bufnr)
+	if client.supports_method("textDocument/formatting") then
+		vim.api.nvim_clear_autocmds({ group = lsp_augroup, buffer = bufnr })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = lsp_augroup,
+			buffer = bufnr,
+			callback = function()
+				lsp_formatting(bufnr)
+			end,
+		})
+	end
 end
 
 null_ls.setup({
-  on_attach = function(client, bufnr)
-    if client.server_capabilities.documentFormattingProvider then
-      vim.api.nvim_command([[augroup Format]])
-      vim.api.nvim_command([[autocmd! * <buffer>]])
-      vim.api.nvim_command([[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]])
-      vim.api.nvim_command([[augroup END]])
-    end
-  end,
-  sources = {
-    null_ls.builtins.diagnostics.eslint_d.with({
-      diagnostics_format = "[eslint] #{m}\n(#{c]})",
-    }),
-    null_ls.builtins.diagnostics.markdownlint,
-    null_ls.builtins.diagnostics.cfn_lint.with({
-      filetypes = { "yml", "yaml", "json" },
-    }),
-    null_ls.builtins.diagnostics.golangci_lint,
-    null_ls.builtins.code_actions.shellcheck,
-    null_ls.builtins.formatting.shfmt,
-    null_ls.builtins.formatting.prettierd.with({
-      filetypes = {
-        "javascript",
-        "javascriptreact",
-        "typescript",
-        "typescriptreact",
-        "vue",
-        "css",
-        "scss",
-        "less",
-        "html",
-        "json",
-        "jsonc",
-        "yml",
-        "yaml",
-        "markdown",
-        "markdown.mdx",
-        "graphql",
-        "handlebars",
-      },
-    }),
-    null_ls.builtins.formatting.isort,
-    null_ls.builtins.formatting.black,
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.gofumpt,
-    null_ls.builtins.formatting.goimports,
-  },
+	on_attach = on_attach,
+	sources = {
+		null_ls.builtins.diagnostics.eslint_d.with({
+			diagnostics_format = "[eslint] #{m}\n(#{c]})",
+		}),
+		null_ls.builtins.diagnostics.markdownlint,
+		null_ls.builtins.diagnostics.cfn_lint.with({
+			filetypes = { "yml", "yaml", "json" },
+		}),
+		null_ls.builtins.diagnostics.golangci_lint,
+		null_ls.builtins.code_actions.shellcheck,
+		null_ls.builtins.formatting.shfmt,
+		null_ls.builtins.formatting.prettierd.with({
+			filetypes = {
+				"javascript",
+				"javascriptreact",
+				"typescript",
+				"typescriptreact",
+				"vue",
+				"css",
+				"scss",
+				"less",
+				"html",
+				"json",
+				"jsonc",
+				"yml",
+				"yaml",
+				"markdown",
+				"markdown.mdx",
+				"graphql",
+				"handlebars",
+			},
+		}),
+		null_ls.builtins.formatting.isort,
+		null_ls.builtins.formatting.black,
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.gofumpt,
+		null_ls.builtins.formatting.goimports,
+		null_ls.builtins.formatting.golines,
+	},
 })
