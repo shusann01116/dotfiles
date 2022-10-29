26baa7e485bbac98e61af8040115a64cb50d1c9a
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2022-10-29 13:13:41 +0900

    add golang linter, formatter, and lsp

--- home/.config/nvim/after/plugin/mason.rc.lua
+++ home/.config/nvim/after/plugin/mason.rc.lua
@@ -1,34 +1,34 @@
 local status, mason = pcall(require, "mason")
 if not status then
-	return
+  return
 end
 local status2, mason_lspconfig = pcall(require, "mason-lspconfig")
 if not status2 then
-	return
+  return
 end
 
 mason.setup({})
 mason_lspconfig.setup({
-	ensure_installed = {
-		"sumneko_lua",
-		"tailwindcss",
-		"csharp_ls",
-		"dagger",
-		"dockerls",
-		"yamlls",
-		"terraformls",
-		"tsserver",
-		"pylsp",
-	},
+  ensure_installed = {
+    "sumneko_lua",
+    "tailwindcss",
+    "csharp_ls",
+    "dagger",
+    "dockerls",
+    "yamlls",
+    "terraformls",
+    "tsserver",
+    "pylsp",
+  },
 })
 
 vim.g.coq_settings = {
-	auto_start = true,
+  auto_start = true,
 }
 
 local status3, coq = pcall(require, "coq")
 if not status3 then
-	return
+  return
 end
 
 local lspconfig = require("lspconfig")
@@ -37,58 +37,59 @@ lspconfig.tailwindcss.setup(coq.lsp_ensure_capabilities())
 lspconfig.dagger.setup(coq.lsp_ensure_capabilities())
 lspconfig.dockerls.setup(coq.lsp_ensure_capabilities())
 lspconfig.yamlls.setup({
-	settings = {
-		coq.lsp_ensure_capabilities(),
-		yaml = {
-			customTags = {
-				"!Base64 scalar",
-				"!Cidr scalar",
-				"!And sequence",
-				"!Equals sequence",
-				"!If sequence",
-				"!Not sequence",
-				"!Or sequence",
-				"!Condition scalar",
-				"!FindInMap sequence",
-				"!GetAtt scalar",
-				"!GetAtt sequence",
-				"!GetAZs scalar",
-				"!ImportValue scalar",
-				"!Join sequence",
-				"!Select sequence",
-				"!Split sequence",
-				"!Sub scalar",
-				"!Transform mapping",
-				"!Ref scalar",
-			},
-			schemas = {
-				["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "**/*docker-compose.yml",
-				["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] = "**/kube-manifests/*",
-			},
-		},
-	},
+  settings = {
+    coq.lsp_ensure_capabilities(),
+    yaml = {
+      customTags = {
+        "!Base64 scalar",
+        "!Cidr scalar",
+        "!And sequence",
+        "!Equals sequence",
+        "!If sequence",
+        "!Not sequence",
+        "!Or sequence",
+        "!Condition scalar",
+        "!FindInMap sequence",
+        "!GetAtt scalar",
+        "!GetAtt sequence",
+        "!GetAZs scalar",
+        "!ImportValue scalar",
+        "!Join sequence",
+        "!Select sequence",
+        "!Split sequence",
+        "!Sub scalar",
+        "!Transform mapping",
+        "!Ref scalar",
+      },
+      schemas = {
+        ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "**/*docker-compose.yml",
+        ["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] = "**/kube-manifests/*",
+      },
+    },
+  },
 })
 lspconfig.terraformls.setup(coq.lsp_ensure_capabilities())
 lspconfig.pylsp.setup(coq.lsp_ensure_capabilities())
 lspconfig.tsserver.setup(coq.lsp_ensure_capabilities({
-	filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
-	cmd = { "typescript-language-server", "--stdio" },
+  filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+  cmd = { "typescript-language-server", "--stdio" },
 }))
 lspconfig.sumneko_lua.setup(coq.lsp_ensure_capabilities({
-	settings = {
-		Lua = {
-			diagnostics = {
-				-- Get the language server to recognize the 'vim' global
-				globals = { "vim" },
-			},
-			workspace = {
-				-- Make the server aware of Neovim runtime files
-				library = vim.api.nvim_get_runtime_file("", true),
-				checkThirdParty = false,
-			},
-		},
-	},
+  settings = {
+    Lua = {
+      diagnostics = {
+        -- Get the language server to recognize the 'vim' global
+        globals = { "vim" },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
+      },
+    },
+  },
 }))
+lspconfig.gopls.setup { coq.lsp_ensure_capabilities() }
 
 vim.cmd([[
   augroup COQ
--- home/.config/nvim/after/plugin/null-ls.rc.lua
+++ home/.config/nvim/after/plugin/null-ls.rc.lua
@@ -1,50 +1,53 @@
 local status, null_ls = pcall(require, "null-ls")
 if not status then
-	return
+  return
 end
 
 null_ls.setup({
-	on_attach = function(client, bufnr)
-		if client.server_capabilities.documentFormattingProvider then
-			vim.api.nvim_command([[augroup Format]])
-			vim.api.nvim_command([[autocmd! * <buffer>]])
-			vim.api.nvim_command([[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]])
-			vim.api.nvim_command([[augroup END]])
-		end
-	end,
-	sources = {
-		null_ls.builtins.diagnostics.eslint_d.with({
-			diagnostics_format = "[eslint] #{m}\n(#{c]})",
-		}),
-		null_ls.builtins.diagnostics.markdownlint,
-		null_ls.builtins.diagnostics.cfn_lint.with({
-			filetypes = { "yml", "yaml", "json" },
-		}),
-		null_ls.builtins.code_actions.shellcheck,
-		null_ls.builtins.formatting.shfmt,
-		null_ls.builtins.formatting.prettierd.with({
-			filetypes = {
-				"javascript",
-				"javascriptreact",
-				"typescript",
-				"typescriptreact",
-				"vue",
-				"css",
-				"scss",
-				"less",
-				"html",
-				"json",
-				"jsonc",
-				"yml",
-				"yaml",
-				"markdown",
-				"markdown.mdx",
-				"graphql",
-				"handlebars",
-			},
-		}),
-		null_ls.builtins.formatting.isort,
-		null_ls.builtins.formatting.black,
-		null_ls.builtins.formatting.stylua,
-	},
+  on_attach = function(client, bufnr)
+    if client.server_capabilities.documentFormattingProvider then
+      vim.api.nvim_command([[augroup Format]])
+      vim.api.nvim_command([[autocmd! * <buffer>]])
+      vim.api.nvim_command([[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]])
+      vim.api.nvim_command([[augroup END]])
+    end
+  end,
+  sources = {
+    null_ls.builtins.diagnostics.eslint_d.with({
+      diagnostics_format = "[eslint] #{m}\n(#{c]})",
+    }),
+    null_ls.builtins.diagnostics.markdownlint,
+    null_ls.builtins.diagnostics.cfn_lint.with({
+      filetypes = { "yml", "yaml", "json" },
+    }),
+    null_ls.builtins.diagnostics.golangci_lint,
+    null_ls.builtins.code_actions.shellcheck,
+    null_ls.builtins.formatting.shfmt,
+    null_ls.builtins.formatting.prettierd.with({
+      filetypes = {
+        "javascript",
+        "javascriptreact",
+        "typescript",
+        "typescriptreact",
+        "vue",
+        "css",
+        "scss",
+        "less",
+        "html",
+        "json",
+        "jsonc",
+        "yml",
+        "yaml",
+        "markdown",
+        "markdown.mdx",
+        "graphql",
+        "handlebars",
+      },
+    }),
+    null_ls.builtins.formatting.isort,
+    null_ls.builtins.formatting.black,
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.formatting.gofumpt,
+    null_ls.builtins.formatting.goimports,
+  },
 })
