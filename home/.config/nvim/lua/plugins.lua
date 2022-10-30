9d21f76e7e8183ef8d77d0c782a2c8d36f1037db
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2022-10-30 15:02:57 +0900

    replace neosolarized with vscode theme

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -8,10 +8,7 @@ vim.cmd([[packadd packer.nvim]])
 
 packer.startup(function(use)
 	use("wbthomason/packer.nvim")
-	use({
-		"svrana/neosolarized.nvim",
-		requires = { "tjdevries/colorbuddy.nvim" },
-	})
+	use("Mofiqul/vscode.nvim")
 	use({
 		"nvim-lualine/lualine.nvim",
 		requires = { "kyazdani42/nvim-web-devicons", opt = true },
