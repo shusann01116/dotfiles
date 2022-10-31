38ffd30fa87b15a29e56a2a84c209f643a63913a
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2022-10-31 17:08:52 +0900

    feat: add go lang support for TS (#33)

--- home/.config/nvim/after/plugin/treesitter.rc.lua
+++ home/.config/nvim/after/plugin/treesitter.rc.lua
@@ -1,43 +1,46 @@
-local status, ts = pcall(require, 'nvim-treesitter.configs')
-if (not status) then return end
+local status, ts = pcall(require, "nvim-treesitter.configs")
+if not status then
+	return
+end
 
 -- This is required to not have cue files marked as `cuesheet`
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-  pattern = { "*.cue" },
-  command = "set filetype=cue"
+	pattern = { "*.cue" },
+	command = "set filetype=cue",
 })
 
-local parse_config = require "nvim-treesitter.parsers".get_parser_configs()
+local parse_config = require("nvim-treesitter.parsers").get_parser_configs()
 parse_config.cue = {
-  install_info = {
-    url = "https://github.com/eonpatapon/tree-sitter-cue",
-    files = { "src/parser.c", "src/scanner.c" },
-    branch = "main"
-  },
-  filetype = "cue",
+	install_info = {
+		url = "https://github.com/eonpatapon/tree-sitter-cue",
+		files = { "src/parser.c", "src/scanner.c" },
+		branch = "main",
+	},
+	filetype = "cue",
 }
 
-ts.setup {
-  highlight = {
-    enable = true,
-    disable = {},
-  },
-  indent = {
-    enable = true,
-    disable = {},
-  },
-  ensure_installed = {
-    'tsx',
-    'lua',
-    'json',
-    'css',
-    'hcl',
-    'cue',
-    'c_sharp',
-    'yaml',
-    'bash',
-  },
-  autotag = {
-    enable = true,
-  }
-}
+ts.setup({
+	highlight = {
+		enable = true,
+		disable = {},
+	},
+	indent = {
+		enable = true,
+		disable = {},
+	},
+	ensure_installed = {
+		"tsx",
+		"lua",
+		"json",
+		"css",
+		"hcl",
+		"cue",
+		"c_sharp",
+		"yaml",
+		"bash",
+		"go",
+	},
+	autotag = {
+		enable = true,
+	},
+})
