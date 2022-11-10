4a22eba3eceee1deee6606cf1817bfb11f876f8c
s-chan <s-chan@amiya.co.jp>
2022-11-10 14:05:05 +0900

    add formatter for hcl

--- home/.config/nvim/after/plugin/null-ls.rc.lua
+++ home/.config/nvim/after/plugin/null-ls.rc.lua
@@ -69,5 +69,6 @@ null_ls.setup({
 		null_ls.builtins.formatting.gofumpt,
 		null_ls.builtins.formatting.goimports,
 		null_ls.builtins.formatting.golines,
+		null_ls.builtins.formatting.packer,
 	},
 })
