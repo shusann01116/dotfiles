659a6ee61d65b73ac2b022ad540d555bdfd9e39a
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2022-12-07 01:48:29 +0900

    chore(test): remove test method

--- pkg/util_test/compute_test.go
+++ pkg/util_test/compute_test.go
@@ -26,23 +26,3 @@ func TestComputerDirHash(t *testing.T) {
 		t.Errorf("error computing hash: %v, %v, want %v", got, err, want)
 	}
 }
-
-// TestGetExePath tests the GetExePath function
-func TestGetExePath(t *testing.T) {
-	// get current directory
-	dir, err := os.Getwd()
-	if err != nil {
-		t.Errorf("error getting current directory: %v", err)
-	}
-
-	// get executable path
-	got, err := util.GetExePath()
-	if err != nil {
-		t.Errorf("error getting executable path: %v", err)
-	}
-
-	// check that executable path is the same as the current directory
-	if got != dir {
-		t.Errorf("executable path (%v) != current directory (%v)", got, dir)
-	}
-}
