d1be53a40b1d09e15bc4a575a4437ba889b31a91
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2023-06-08 00:06:05 +0900

    Add jj to Esc

--- home/.config/nvim/lua/maps.lua
+++ home/.config/nvim/lua/maps.lua
@@ -34,3 +34,5 @@ keymap.set("n", "<C-w><left>", "<C-w><")
 keymap.set("n", "<C-w><right>", "<C-w>>,")
 keymap.set("n", "<C-w><up>", "<C-w>+")
 keymap.set("n", "<C-w><down>", "<C-w>-")
+
+keymap.set("i", "jj", "<Esc>")
