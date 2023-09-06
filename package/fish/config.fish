82e9fa5055a298f41aa6d8e2b3aaa26094a1af12
Shutaro Chan <26602565+shusann01116@users.noreply.github.com>
2023-09-06 09:06:32 +0900

    feat: add fish config (#69)
    Signed-off-by: shusann01116 <26602565+shusann01116@users.noreply.github.com>

---
+++ package/fish/config.fish
@@ -0,0 +1,26 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set PATH $PATH:/sbin
+    set PATH $PATH:/usr/local/bin
+    set PATH $PATH:$HOME/.local/bin
+    set BROWSER /usr/bin/wslview
+    starship init fish | source
+
+    fish_vi_key_bindings
+
+    # AWS CLI completion
+    function __fish_complete_aws
+        env COMP_LINE=(commandline -pc) aws_completer | tr -d ' '
+    end
+
+    complete -c aws -f -a "(__fish_complete_aws)"
+
+    # alias
+    alias v=nvim
+    alias t=terraform
+    alias l=lazygit
+    alias g=git
+    alias k=kubectl
+end
+
+export GPG_TTY=$(tty)
