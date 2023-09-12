4b7311eade2e411b8364ba9209ec524d76320cb9
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2023-09-12 22:27:34 +0900

    fix: homebrew not found from fish

--- package/fish/config.fish
+++ package/fish/config.fish
@@ -1,26 +1,30 @@
+if test $(uname) = "Darwin"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+end
+
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    set PATH $PATH:/sbin
-    set PATH $PATH:/usr/local/bin
-    set PATH $PATH:$HOME/.local/bin
-    set BROWSER /usr/bin/wslview
-    starship init fish | source
+  # Commands to run in interactive sessions can go here
+  set PATH $PATH:/sbin
+  set PATH $PATH:/usr/local/bin
+  set PATH $PATH:$HOME/.local/bin
+  set BROWSER /usr/bin/wslview
+  starship init fish | source
 
-    fish_vi_key_bindings
+  fish_vi_key_bindings
 
-    # AWS CLI completion
-    function __fish_complete_aws
-        env COMP_LINE=(commandline -pc) aws_completer | tr -d ' '
-    end
+  # AWS CLI completion
+  function __fish_complete_aws
+      env COMP_LINE=(commandline -pc) aws_completer | tr -d ' '
+  end
 
-    complete -c aws -f -a "(__fish_complete_aws)"
+  complete -c aws -f -a "(__fish_complete_aws)"
 
-    # alias
-    alias v=nvim
-    alias t=terraform
-    alias l=lazygit
-    alias g=git
-    alias k=kubectl
+  # alias
+  alias v=nvim
+  alias t=terraform
+  alias l=lazygit
+  alias g=git
+  alias k=kubectl
 end
 
 export GPG_TTY=$(tty)
