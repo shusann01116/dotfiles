a3ce22fa9e792be827e42040425d9e808944a8f4
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2025-08-26 21:53:46 +0900

    Fallback for Japanese glyph

--- package/wezterm/.wezterm.lua
+++ package/wezterm/.wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font_size = 13
-config.font = wezterm.font("JetBrains Mono")
+config.font = wezterm.font_with_fallback({ "JetBrains Mono", "Noto Sans JP" })
 config.default_prog = { "/bin/zsh", "-l", "-c", "tmux a -t 0 || tmux" }
 config.enable_tab_bar = false
 config.color_scheme = "Catppuccin Mocha"
